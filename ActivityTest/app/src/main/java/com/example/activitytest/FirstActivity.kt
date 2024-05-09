c3bd146132c8baa12468137142b0730930d1835c
Sadhu <1714222385@qq.com>
2024-05-09 10:58:10 +0800

    Activity 创建 menu

--- ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -2,6 +2,8 @@ package com.example.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import com.example.activitytest.databinding.FirstLayoutBinding
@@ -24,9 +26,27 @@ class FirstActivity : AppCompatActivity() {
 //        ViewBinding 写法
         binding = FirstLayoutBinding.inflate(layoutInflater)
         binding.btn1.setOnClickListener {
-            val t = Toast.makeText(this, "点了按钮一", Toast.LENGTH_SHORT)
-            t.show()
+            showToast("点了按钮一")
         }
         setContentView(binding.root);
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // 给当前 Activity 创建菜单
+        menuInflater.inflate(R.menu.main, menu);
+        // return true 表示希望显示出来
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> showToast("add menu item")
+            R.id.remove_item -> showToast("remove menu item")
+        }
+        // return true 表示希望显示出来
+        return true
     }
 }
