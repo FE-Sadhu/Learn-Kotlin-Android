89a5b0540ff40b6b87eb7f1806b92ace035c5fca
Sadhu <1714222385@qq.com>
2024-05-30 14:43:42 +0800

    查询数据库数据

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.kt
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.databasetest
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -61,6 +64,32 @@ class MainActivity : AppCompatActivity() {
             val db = dbHelper.writableDatabase
             // 删除 id 大于 2 的数据
             db.delete("Book", "id > ?", arrayOf("2"))
+        }
+
+        val queryData = findViewById<Button>(R.id.query_data)
+        queryData.setOnClickListener {
+            val db = dbHelper.readableDatabase
+            // 第一个参数不用说，当然还是表名，表示我们希望从哪张表中查询数据。
+            // 第二个参数用于指定去查询哪几列，如果不指定则默认查询所有列。
+            // 第三、第四个参数用于约束查询某一行或某几行的数据，不指定则默认查询所有行的数据。
+            // 第五个参数用于指定需要去 group by的列，不指定则表示不对查询结果进行groupby操作。
+            // 第六个参数用于对group by之后的数据进行进一步的过滤，不指定则表示不进行过滤。
+            // 第七个参数用于指定查询结果的排序方式，不指定则表示使用默认的排序方式。
+            val cursor = db.query("Book", null, null, null, null, null, null, null)
+            // 将指针移动到第一行
+            if (cursor.moveToFirst()) {
+                 do {
+                     val name = cursor.getString(cursor.getColumnIndex("name"))
+                     val author = cursor.getString(cursor.getColumnIndex("author"))
+                     val pages = cursor.getInt(cursor.getColumnIndex("pages"))
+                     val price = cursor.getDouble(cursor.getColumnIndex("price"))
+                     Log.d("MainActivity", "书名 $name")
+                     Log.d("MainActivity", "作者 $author")
+                     Log.d("MainActivity", "页数 $pages")
+                     Log.d("MainActivity", "价格 $price")
+                 } while (cursor.moveToNext()) // 一行一行读
+            }
+            cursor.close()
         }
     }
 }
