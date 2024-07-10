e11ec7aeda8749dfd8debb98e75062731e5f62a8
Sadhu <39456032+FE-Sadhu@users.noreply.github.com>
2024-07-10 16:40:18 +0800

    HttpURLConnection

---
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.networktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.sendRequestBtn)
+        btn.setOnClickListener {
+            sendRequestWithHttpURLConnection()
+        }
+    }
+
+    private fun sendRequestWithHttpURLConnection() {
+        // 开启线程发起网络请求
+        thread {
+            var connection: HttpURLConnection? = null;
+            try {
+                val response = StringBuilder()
+                val url = URL("https://www.baidu.com")
+                connection = url.openConnection() as HttpURLConnection;
+                connection.connectTimeout = 8000
+                connection.readTimeout = 8000
+                val input = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                // 关闭 Http 连接
+                connection?.disconnect()
+            }
+        }
+    }
+
+    private fun showResponse(response: String) {
+        // 不允许在子线程进行 UI 操作
+        // runOnUiThread() 方法其实就是对异步消息处理机制进行了一层封装
+        runOnUiThread {
+            val textView = findViewById<TextView>(R.id.responseText)
+            textView.text = response
+        }
+    }
+}
