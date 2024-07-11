bf8682597c73c7ba0798b33943e5b3181764501f
Sadhu <39456032+FE-Sadhu@users.noreply.github.com>
2024-07-11 14:06:41 +0800

    retrofit send request

---
+++ RetrotTest/app/src/main/java/com/example/retrottest/App.kt
@@ -0,0 +1,3 @@
+package com.example.retrottest
+
+class App(val id: String, val name: String, val version: String)
--- RetrotTest/app/src/main/java/com/example/retrottest/MainActivity.kt
+++ RetrotTest/app/src/main/java/com/example/retrottest/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.retrottest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Callback
+import retrofit2.Call
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendRequest() {
+        val appService = ServiceCreator.create<AppService>()
+        appService.getAppData().enqueue(object : Callback<List<App>> {
+            override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
+                val list = response.body()
+                if (list != null) {
+                    for (app in list) {
+                        Log.d("MainActivity", "id is ${app.id}")
+                        Log.d("MainActivity", "name is ${app.name}")
+                        Log.d("MainActivity", "version is ${app.version}")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<App>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
